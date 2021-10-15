28a5c167a39ff9145030797518ee27e67eeef4df
MarkBrooks3 <78883486+MarkBrooks3@users.noreply.github.com>
2021-10-15 13:16:46 -0500

    worked on some styling

--- src/components/PokemonLink.js
+++ src/components/PokemonLink.js
@@ -2,7 +2,29 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { addPokemon } from '../reducers/pokemons';
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 175px;
+  height: 200px;
+  background-color: #f0f0f0;
+  border-radius: 20px;
+  margin: 5px;
+  text-decoration: none;
+  color: black;
+  font-family: VT323;
+`;
+
+const StyledImg = styled.img`
+  height: 150px;
+  background-color: white;
+  border-radius: 20px 20px 0px 0px;
+`;
 
 const PokemonLink = ({ name }) => {
   const dispatch = useDispatch();
@@ -17,9 +39,11 @@ const PokemonLink = ({ name }) => {
   }, [pokemon, name, dispatch]);
 
   return pokemon ? (
-    <Link to={`/pokemon/${pokemon.id}`}>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-    </Link>
+    <StyledLink to={`/pokemon/${pokemon.id}`}>
+      <StyledImg src={pokemon.sprites.front_default} alt={pokemon.name} />
+      {pokemon.name}
+      {console.log(pokemon)}
+    </StyledLink>
   ) : (
     <div />
   );
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { changeGroup } from '../reducers/group';
 import PokemonLink from './PokemonLink';
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 1000px;
+`;
 
 const PokemonList = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state.group);
 
   return state.results ? (
-    <div>
+    <StyledDiv>
       {state.results.map((p) => (
         <PokemonLink key={p.name} name={p.name} />
       ))}
@@ -16,7 +23,7 @@ const PokemonList = () => {
         Previous
       </button>
       <button onClick={() => dispatch(changeGroup(state.next))}>Next</button>
-    </div>
+    </StyledDiv>
   ) : (
     <div>Loading...</div>
   );
