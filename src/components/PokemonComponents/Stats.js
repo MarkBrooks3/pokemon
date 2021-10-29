0b3d83fb105c6d7eb773f2a9329cdc6538038c59
MarkBrooks3 <78883486+MarkBrooks3@users.noreply.github.com>
2021-10-28 23:25:09 -0500

    started the bars for stats

--- src/components/PokemonComponents/Stats.js
+++ src/components/PokemonComponents/Stats.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+//TODO add more styles
+
+const StatBar = styled.div`
+  width: ${(props) => (props.value / 255) * 100}%;
+  background-color: red;
+  border-radius: 0.3em;
+`;
+
+const BarHolder = styled.div`
+  width: 255px;
+  background-color: gray;
+  border-radius: 0.3em;
+`;
 
 const Stats = ({ stats }) => {
   return (
@@ -6,7 +20,10 @@ const Stats = ({ stats }) => {
       <h3>Stats</h3>
       {stats.map((s) => (
         <li key={s.stat.name}>
-          {s.stat.name} {s.base_stat}
+          {s.stat.name}{' '}
+          <BarHolder>
+            <StatBar value={s.base_stat}>{s.base_stat}</StatBar>
+          </BarHolder>
         </li>
       ))}
     </div>
