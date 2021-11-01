7bfb506927d05761bb93628a09be8168d26827e5
MarkBrooks3 <78883486+MarkBrooks3@users.noreply.github.com>
2021-11-01 18:14:28 -0500

    finished stats

--- src/components/PokemonComponents/Stats.js
+++ src/components/PokemonComponents/Stats.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-//TODO add more styles
+
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  border-bottom: hsl(0, 0%, 95%) 1px solid;
+`;
+
+const StyledRow = styled.tr`
+  height: 2em;
+  border-top: hsl(0, 0%, 95%) 1px solid;
+`;
+
+const BarCell = styled.td`
+  width: 255px;
+`;
+
+const TitleCell = styled.td`
+  text-align: right;
+  width: 6em;
+  color: hsl(0, 0%, 40%);
+`;
+
+const NumberCell = styled.td`
+  text-align: right;
+  padding: 4px 10px;
+`;
 
 const StatBar = styled.div`
   width: ${(props) => (props.value / 255) * 100}%;
@@ -26,38 +50,37 @@ const StatBar = styled.div`
   border: 1px solid rgba(0, 0, 0, 0.15);
 `;
 
-const BarCell = styled.td`
-  width: 255px;
-`;
-
-const TitleCell = styled.td`
-  text-align: right;
-  width: 5em;
-`;
-
-const NumberCell = styled.td`
-  width: 2em;
-`;
-
-const StyledTable = styled.table`
-  border-collapse: collapse;
-`;
+const formatName = (name) => {
+  name = name.replace('special-', 'Sp. ');
+  return name
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+};
 
 const Stats = ({ stats }) => {
+  let total = 0;
   return (
     <div>
-      <h3>Stats</h3>
+      <h3>Base Stats</h3>
       <StyledTable>
         <tbody>
-          {stats.map((s) => (
-            <tr key={s.stat.name}>
-              <TitleCell>{s.stat.name}</TitleCell>
-              <NumberCell>{s.base_stat}</NumberCell>
-              <BarCell>
-                <StatBar value={s.base_stat} />
-              </BarCell>
-            </tr>
-          ))}
+          {stats.map((s, i) => {
+            total += s.base_stat;
+            return (
+              <StyledRow key={s.stat.name} i={i}>
+                <TitleCell>{formatName(s.stat.name)}</TitleCell>
+                <NumberCell>{s.base_stat}</NumberCell>
+                <BarCell>
+                  <StatBar value={s.base_stat} />
+                </BarCell>
+              </StyledRow>
+            );
+          })}
+          <StyledRow>
+            <TitleCell>Total</TitleCell>
+            <NumberCell>{total}</NumberCell>
+          </StyledRow>
         </tbody>
       </StyledTable>
     </div>
